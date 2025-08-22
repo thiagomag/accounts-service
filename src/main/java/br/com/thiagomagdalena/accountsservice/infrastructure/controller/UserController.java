fc8594d2367a4d6efc51c2f1ec7f5cb4a0e5bd2f
thiagomagdalena <thiagomagdalena@gmail.com>
2025-08-21 23:12:34 -0300

    correções

--- src/main/java/br/com/thiagomagdalena/accountsservice/infrastructure/controller/UserController.java
+++ src/main/java/br/com/thiagomagdalena/accountsservice/infrastructure/controller/UserController.java
@@ -1,8 +1,20 @@
 package br.com.thiagomagdalena.accountsservice.infrastructure.controller;
 
-import br.com.thiagomagdalena.accountsservice.application.interfaces.user.*;
+import br.com.thiagomagdalena.accountsservice.application.interfaces.user.ActivateSubscriptionUseCase;
+import br.com.thiagomagdalena.accountsservice.application.interfaces.user.AuthenticateUserUseCase;
+import br.com.thiagomagdalena.accountsservice.application.interfaces.user.CreateUserUseCase;
+import br.com.thiagomagdalena.accountsservice.application.interfaces.user.DeleteUserUseCase;
+import br.com.thiagomagdalena.accountsservice.application.interfaces.user.GetLoggedUserUseCase;
+import br.com.thiagomagdalena.accountsservice.application.interfaces.user.GetUserUseCase;
+import br.com.thiagomagdalena.accountsservice.application.interfaces.user.GetUsersUseCase;
+import br.com.thiagomagdalena.accountsservice.application.interfaces.user.UpdateUserUseCase;
 import br.com.thiagomagdalena.accountsservice.infrastructure.controller.adapters.UserAdapter;
-import br.com.thiagomagdalena.accountsservice.infrastructure.controller.dto.user.*;
+import br.com.thiagomagdalena.accountsservice.infrastructure.controller.dto.user.CreateUserDto;
+import br.com.thiagomagdalena.accountsservice.infrastructure.controller.dto.user.LoginUserDto;
+import br.com.thiagomagdalena.accountsservice.infrastructure.controller.dto.user.RecoveryJwtTokenDto;
+import br.com.thiagomagdalena.accountsservice.infrastructure.controller.dto.user.SubscriptionDto;
+import br.com.thiagomagdalena.accountsservice.infrastructure.controller.dto.user.UpdateUserDto;
+import br.com.thiagomagdalena.accountsservice.infrastructure.controller.dto.user.UserResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -10,7 +22,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -21,6 +42,7 @@ import java.util.List;
 public class UserController {
 
     private final AuthenticateUserUseCase authenticateUserUseCase;
+    private final GetLoggedUserUseCase getLoggedUserUseCase;
     private final CreateUserUseCase createUserUseCase;
     private final GetUsersUseCase getUsersUseCase;
     private final GetUserUseCase getUserUseCase;
@@ -33,6 +55,13 @@ public class UserController {
     @Operation(summary = "Autenticar usuário", description = "Endpoint para autenticar um usuário e retornar um token JWT")
     public ResponseEntity<RecoveryJwtTokenDto> authenticateUser(@Valid @RequestBody LoginUserDto loginUserDto) {
         return ResponseEntity.ok(authenticateUserUseCase.execute(loginUserDto));
+    }
+
+    @GetMapping("/me")
+    @PreAuthorize("isAuthenticated()")
+    @Operation(summary = "Retorna os dados do usuário logado")
+    public ResponseEntity<UserResponse> getLoggedUser(@RequestHeader(value = "X-User-Id") Long xUserId) {
+        return ResponseEntity.ok(getLoggedUserUseCase.execute(xUserId));
     }
 
     @PostMapping
